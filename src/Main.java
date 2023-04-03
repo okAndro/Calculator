b2df69b26e75662bd8cf73a132520a3cd53573fb
okAndro <129780072+okAndro@users.noreply.github.com>
2023-04-03 21:50:02 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,38 +1,38 @@
 import java.util.Scanner;
 public class Main
 {
-    private static final String[] ROMANUNITS = new String[] {"","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}; // римские числа от 1 до 10 (единицы)
-    private static final String[] ROMANTENS = new String[] {"","X", "XX", "XXX", "LX", "L", "LX", "LXX", "LXXX", "XC", "C"}; // римские числа от 10 до 100 (десятки)
+    private static final String[] ROMANUNITS = new String[] {"","I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX", "X"}; 
+    private static final String[] ROMANTENS = new String[] {"","X", "XX", "XXX", "LX", "L", "LX", "LXX", "LXXX", "XC", "C"}; 
     public static void main(String[] args) {
         while (true){
         try{
             Scanner in = new Scanner(System.in);
             System.out.println("↓ Введите выражение строкой ниже (например, 2 + 3 или VI / III). Можно вводить с пробелом между операндами или без ↓:");
-            System.out.println(calc(in.nextLine())); // чтение строки из консоли, запуск метода calc
+            System.out.println(calc(in.nextLine())); 
         }
         catch(Exception e){
-            System.out.println(e.getMessage()); // вывод ошибок
+            System.out.println(e.getMessage());
         }
     }}
     public static String calc(String input) throws Exception {
-        String[] parts = input.replaceAll("\\s", "").split("(?<=[-+*/])|(?=[-+*/])");// разбивает строку input на массив строк, используя регулярное выражение
+        String[] parts = input.replaceAll("\\s", "").split("(?<=[-+*/])|(?=[-+*/])");
         if((parts.length != 3)){
             throw new Exception("Привет\nЧтобы работало, нужно ввести два операнда и один оператор (+, -, /, *).");
         }
-        int a = tryParse(parts[0]); // пытаемся перевести первое число в стандартный вид
-        String operator = parts[1]; // оператор
-        int b = tryParse(parts[2]); // пытаемся перевести второе число в стандартный вид
+        int a = tryParse(parts[0]); 
+        String operator = parts[1]; 
+        int b = tryParse(parts[2]); 
         int answ = 0; //ответ
-        boolean isRoman = false; // true - если римские числа, в противном случае - false
-        if((a == 0) && (b == 0)){ // проверка на не арабские цифры
-            a = romanToArabic(parts[0]); // пытаемся первести первое число в римское
-            b = romanToArabic(parts[2]); // пытаемся первести второе число в римское
+        boolean isRoman = false; 
+        if((a == 0) && (b == 0)){ 
+            a = romanToArabic(parts[0]); 
+            b = romanToArabic(parts[2]); 
             isRoman = true;
         }
-        if(((a == 0) || (b == 0)) || ((a > 10) || (b > 10))){ //  проверка на одинаковую систему счисления и принадлежность диапозону [1..10]
+        if(((a == 0) || (b == 0)) || ((a > 10) || (b > 10))){ 
             throw new Exception("throws Exception //т.к. используются одновременно разные системы счисления\nОба операндов должны быть одной системы счисления, а также принадлежать диапозону [1..10].");
         }
-        switch(operator){ // выбор действия относительно оператора
+        switch(operator){ 
             case "+":
                 answ = a + b;
                 break;
@@ -45,33 +45,33 @@ public class Main
             case "/":
                 answ = a / b;
                 break;
-            default: //если такого оператора нет
+            default: 
                 throw new Exception("throws Exception //т.к. оператор должен быть (+, -, /, *).");
         }
-        if(isRoman && answ < 1){ // проверка на ответ меньше 1 (в случае с римскими цифрами)
+        if(isRoman && answ < 1){ 
             throw new Exception("throws Exception //т.к. в римской системе нет отрицательных чисел и нуля");
         }
-        return "Ответ: " + (isRoman ? arabicToRoman(answ) : Integer.toString(answ)); // возвращаем ответ относительно системе счисления
+        return "Ответ: " + (isRoman ? arabicToRoman(answ) : Integer.toString(answ)); 
 
 
     }
-    private static int tryParse(String value){ // перевод строки в число
+    private static int tryParse(String value){ 
         try {
-            return Integer.parseInt(value); // перевод удался
+            return Integer.parseInt(value); 
         }
         catch (NumberFormatException e) {
-            return 0; // перевод не удался
+            return 0; 
         }
     }
-    private static int romanToArabic(String value){ // перевод из римских в арабские цифры
-        for(int i = 1; i < ROMANUNITS.length; i++){ // поиск значения value по массиму ROMANUNITS
+    private static int romanToArabic(String value){ 
+        for(int i = 1; i < ROMANUNITS.length; i++){ 
             if(ROMANUNITS[i].equals(value)){
-                return  i; // перевод удался
+                return  i; 
             }
         }
-        return 0; // перевод не удался
+        return 0; 
     }
-    private static String arabicToRoman(int value){ //перевод из арабских в римские цифры
-        return ROMANTENS[value / 10] + ROMANUNITS[value % 10]; // получаем десятки и единицы, объединяем
+    private static String arabicToRoman(int value){ 
+        return ROMANTENS[value / 10] + ROMANUNITS[value % 10]; 
     }
 }
